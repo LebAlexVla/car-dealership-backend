0c4495a9f3093b23de39eafefd11a996e057c241
Alexander Lebedev <lebalexvla@gmail.com>
2026-02-28 10:52:24 +0300

    feat(domain): add test drive request

---
+++ src/main/java/ru/lebedev/dealership/domain/testdrive/TestDriveRequest.java
@@ -0,0 +1,12 @@
+package ru.lebedev.dealership.domain.testdrive;
+
+import ru.lebedev.dealership.domain.car.valueobjects.CarVersionId;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TestDriveRequest(
+        UUID clientId,
+        CarVersionId carVersionId,
+        LocalDateTime dateTime
+) {}
