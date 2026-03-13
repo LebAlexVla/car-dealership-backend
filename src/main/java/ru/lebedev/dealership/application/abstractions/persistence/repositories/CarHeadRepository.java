8ddba2937eb7c50f6db9a36148f141e4bb85fa7f
Alexander Lebedev <lebalexvla@gmail.com>
2026-03-13 22:22:52 +0300

    feat: add car delete service

--- src/main/java/ru/lebedev/dealership/application/abstractions/persistence/repositories/CarHeadRepository.java
+++ src/main/java/ru/lebedev/dealership/application/abstractions/persistence/repositories/CarHeadRepository.java
@@ -2,14 +2,15 @@ package ru.lebedev.dealership.application.abstractions.persistence.repositories;
 
 import ru.lebedev.dealership.application.abstractions.persistence.queries.CarHeadFilter;
 import ru.lebedev.dealership.domain.car.entities.CarHead;
-import ru.lebedev.dealership.domain.car.vo.CarHeadId;
 
 import java.util.List;
 
 public interface CarHeadRepository {
     CarHead save(CarHead carHead);
 
-    CarHead findById(CarHeadId carHeadId);
+    void delete(long carHeadId);
+
+    CarHead findById(long carHeadId);
 
     List<CarHead> findByFilter(CarHeadFilter filter);
 }
--- src/main/java/ru/lebedev/dealership/application/abstractions/persistence/repositories/CarVersionRepository.java
+++ src/main/java/ru/lebedev/dealership/application/abstractions/persistence/repositories/CarVersionRepository.java
@@ -2,14 +2,15 @@ package ru.lebedev.dealership.application.abstractions.persistence.repositories;
 
 import ru.lebedev.dealership.application.abstractions.persistence.queries.CarVersionFilter;
 import ru.lebedev.dealership.domain.car.entities.CarVersion;
-import ru.lebedev.dealership.domain.car.vo.CarVersionId;
 
 import java.util.List;
 
 public interface CarVersionRepository {
     CarVersion save(CarVersion carVersion);
 
-    CarVersion findById(CarVersionId carVersionId);
+    void delete(long carVersionId);
+
+    CarVersion findById(long carVersionId);
 
     List<CarVersion> findByFilter(CarVersionFilter filter);
 }
