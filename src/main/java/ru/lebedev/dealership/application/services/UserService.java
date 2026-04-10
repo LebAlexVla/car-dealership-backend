a62f0bc6b2435b48ae10fa60fa5bca3cb6902100
Alexander Lebedev <lebalexvla@gmail.com>
2026-04-10 23:04:10 +0300

    feat: add user service

---
+++ src/main/java/ru/lebedev/dealership/application/services/UserService.java
@@ -0,0 +1,34 @@
+package ru.lebedev.dealership.application.services;
+
+import org.springframework.stereotype.Service;
+import ru.lebedev.dealership.domain.user.User;
+import ru.lebedev.dealership.infrastructure.persistence.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Long create(User user) {
+        User savedUser = userRepository.save(user);
+        return savedUser.getId();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
