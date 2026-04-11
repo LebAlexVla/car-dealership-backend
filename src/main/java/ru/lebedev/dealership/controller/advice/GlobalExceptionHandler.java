80eaa56339c3dcd85ccd555bb1d0d2086becb2f8
Alexander Lebedev <lebalexvla@gmail.com>
2026-04-11 05:41:59 +0300

    feat: add exception handler

---
+++ src/main/java/ru/lebedev/dealership/controller/advice/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package ru.lebedev.dealership.controller.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.lebedev.dealership.application.exceptions.ApplicationException;
+import ru.lebedev.dealership.domain.exceptions.DomainException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<String> handleApplication(ApplicationException ex) {
+        if (ex.getClass().getSimpleName().endsWith("NotFoundException")) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DomainException.class)
+    public ResponseEntity<String> handleDomain(DomainException ex) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException() {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal error");
+    }
+}
