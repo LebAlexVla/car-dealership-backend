cc37992fb7a8b78dd959e36d6024aa8fe8632b89
Alexander Lebedev <lebalexvla@gmail.com>
2026-04-11 13:58:56 +0300

    test: fix database connection properties

--- src/integrationTest/java/ru/lebedev/dealership/infrastructure/persistence/repository/AbstractRepositoryDatabaseIT.java
+++ src/integrationTest/java/ru/lebedev/dealership/infrastructure/persistence/repository/AbstractRepositoryDatabaseIT.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,6 +31,7 @@ import java.util.Set;
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 @Testcontainers(disabledWithoutDocker = true)
 @Transactional
 public abstract class AbstractRepositoryDatabaseIT {
